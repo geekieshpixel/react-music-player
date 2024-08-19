5413ca6a4e9bb763a40dedeb6273f9475fc4cd4a
geekieshpixel <162988559+geekieshpixel@users.noreply.github.com>
2024-08-19 13:49:21 +0530

    Top Tracks tab updated

--- src/App.js
+++ src/App.js
@@ -8,51 +8,63 @@ import axios from 'axios';
 import spotifyLogoImg from './assets/spotify-logo.png'; 
 
 const App = () => {
-	const [songs, setSongs] = useState([]);
-	const [currentSong, setCurrentSong] = useState(null);
-	const [tab, setTab] = useState('For You');
-	const [searchQuery, setSearchQuery] = useState('');
+    const [songs, setSongs] = useState([]);
+    const [currentSong, setCurrentSong] = useState(null);
+    const [tab, setTab] = useState('For You');
+    const [searchQuery, setSearchQuery] = useState('');
 
-	useEffect(() => {
-		const fetchSongs = async () => {
-			try {
-				const response = await axios.get('https://cms.samespace.com/items/songs');
-				console.log(response.data.data);
-				setSongs(response.data.data);
-				setCurrentSong(response.data.data[0]);
-			} catch (error) {
-				console.error('Error fetching songs:', error);
-			}
-		};
+    useEffect(() => {
+        const fetchSongs = async () => {
+            try {
+                const response = await axios.get('https://cms.samespace.com/items/songs');
+                console.log(response.data.data);
+                setSongs(response.data.data);
+                setCurrentSong(response.data.data[0]);
+            } catch (error) {
+                console.error('Error fetching songs:', error);
+            }
+        };
 
-		fetchSongs();
-	}, []);
+        fetchSongs();
+    }, []);
 
-	const handleSearch = (query) => {
-		setSearchQuery(query.toLowerCase());
-	};
+    const handleSearch = (query) => {
+        setSearchQuery(query.toLowerCase());
+    };
 
-	const filteredSongs = songs.filter(song => song && song.name && song.artist &&
-		(song.name.toLowerCase().includes(searchQuery) || song.artist.toLowerCase().includes(searchQuery)));
+    const topTracks = songs.filter(song => song.top_track);
 
-	return (
-		<div className="app">
-			<div>
-				<img className="spotify-logo" src={spotifyLogoImg} alt="Spotify Logo Image"/>
-			</div>
-			<div>
-				<Tabs currentTab={tab} onTabChange={setTab} />
-				<SearchBar onSearch={handleSearch} />
-				<SongList
-					songs={filteredSongs}
-					currentSong={currentSong}
-					onSelectSong={setCurrentSong}
-				/>
-			</div>
-			{currentSong &&
-				<div class="player-container"><Player song={currentSong} /></div>}
-		</div>
-	);
+    const filteredSongs = songs.filter(song => song && song.name && song.artist &&
+        (song.name.toLowerCase().includes(searchQuery) || song.artist.toLowerCase().includes(searchQuery)));
+
+    return (
+        <div className="app">
+            <div>
+                <img className="spotify-logo" src={spotifyLogoImg} alt="Spotify Logo Image"/>
+            </div>
+            <div>
+                <Tabs currentTab={tab} onTabChange={setTab} />
+                <SearchBar onSearch={handleSearch} />
+                
+                {tab === 'For You' && (
+                    <SongList
+                        songs={filteredSongs}
+                        currentSong={currentSong}
+                        onSelectSong={setCurrentSong}
+                    />
+                )}
+                {tab === 'Top Tracks' && (
+                    <SongList
+                        songs={topTracks}
+                        currentSong={currentSong}
+                        onSelectSong={setCurrentSong}
+                    />
+                )}
+            </div>
+            {currentSong &&
+                <div className="player-container"><Player song={currentSong} /></div>}
+        </div>
+    );
 };
 
 export default App;
